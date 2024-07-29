343f60fd3861db433a64abd5c93b06080b98a974
paolocatalan <paolo_catalan@yahoo.com>
2024-07-30 01:33:22 +0800

    Added automated testing for tasks model

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SubtasksController;
 use App\Http\Controllers\TasksController;
 use App\Http\Resources\TaskResource;
-use App\Models\Role;
 use App\Models\Task;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Gate;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -18,9 +17,9 @@ Route::middleware(['auth:sanctum', 'last_active_at'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('tasks', TasksController::class);
     Route::apiResource('tasks.subtasks', SubtasksController::class);
-    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::prefix('v2')->group(function () {
@@ -32,6 +31,7 @@ Route::prefix('v2')->group(function () {
     Route::get('/tasks/{task}', function(Task $task) {
         return new TaskResource($task);
     });
+
 
 });
 
